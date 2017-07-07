ec2a6a39304ddf017db8b20a59d5603e1979c67c
Aleksandr Guidrevitch <aguidrevitch@gmail.com>
2017-07-07 17:05:59 +0300

    nginx plugin improvements

--- cachepurge.php
+++ cachepurge.php
@@ -117,6 +117,38 @@ namespace CachePurge {
         }
     }
 
+    /* example nxing configuration section
+
+    location ~ /purge(.*) {
+        selective_cache_purge_query "$1";
+    }
+
+    location ~ /mpurge {
+        client_body_buffer_size 128k;
+        client_max_body_size 128k;
+
+        content_by_lua_block {
+            ngx.req.read_body()
+            local data = ngx.req.get_body_data()
+
+            local http = require "resty.http"
+            local httpc = http.new()
+            httpc:set_timeouts(100, 100, 5000)
+
+            for str in string.gmatch(data, "([^\r\n]+)") do
+                local res, err = httpc:request_uri("http://127.0.0.1/purge" .. str)
+                if not res then
+                    ngx.say("failed to purge " .. str .. ": ", err)
+                else
+                    ngx.say(res.body)
+                end
+            end
+
+            return
+        }
+    }
+     */
+
     /**
      * Class CloudFront
      * @package CachePurge
@@ -143,7 +175,7 @@ namespace CachePurge {
             $this->password = $value;
         }
 
-        public function callApi($url)
+        public function callApi($url, $body)
         {
             if (!$this->url) {
                 error_log("Nginx API is not configured properly");
@@ -151,12 +183,13 @@ namespace CachePurge {
             }
 
             $host = parse_url($url, PHP_URL_HOST);
-            $msg = "GET {$url} HTTP/1.0\r\n";
-            $msg .= "Connection: keep-alive\r\n";
-            $msg .= "Host: {$host}\r\n\r\n";
+            $msg = "POST {$url} HTTP/1.0\r\n";
+            $msg .= "Host: {$host}\r\n";
+            $msg .= "Content-Length: " . strlen($body) . "\r\n";
             if ($this->username)
                 $msg .= 'Authorization: Basic ' . base64_encode($this->username . ':' . $this->password) . "\r\n";
             $msg .= "\r\n";
+            $msg .= $body;
 
             $fp = strtolower(parse_url($url, PHP_URL_SCHEME)) == 'https'
                 ? pfsockopen('ssl://' . $host, 443, $errno, $errstr, $this->timeout)
@@ -166,7 +199,7 @@ namespace CachePurge {
                 fwrite($fp, $msg);
                 $resp = '';
                 while (!feof($fp)) {
-                    $resp .= fread($fp, 65536);
+                    $resp .= fread($fp, 131072);
                 }
                 fclose($fp);
                 return $resp;
@@ -178,11 +211,9 @@ namespace CachePurge {
         public function invalidate(array $urls)
         {
             $result = true;
-            foreach ($urls as $url) {
-                foreach (preg_split('/\s*;\s*/', $this->url) as $apiUrl) {
-                    if (false === ($resp = $this->callApi($apiUrl . $url))) {
-                        $result = false;
-                    }
+            foreach (preg_split('/\s*;\s*/', $this->url) as $apiUrl) {
+                if (false === ($resp = $this->callApi($apiUrl, join("\n", $urls)))) {
+                    $result = false;
                 }
             }
             return $result;
